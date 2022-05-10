97d1ffc16396624a1a0abfc4b822bfa543d7080b
GolubeykovAnton <ds6580@yandex.ru>
2022-05-10 13:15:30 +0300

    Initial Commit

---
+++ recipesBook/recipesBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipesBookApp.swift
+//  recipesBook
+//
+//  Created by Антон Голубейков on 10.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct recipesBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
