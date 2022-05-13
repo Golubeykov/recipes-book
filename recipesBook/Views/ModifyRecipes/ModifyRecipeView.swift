9ca710cea7b640b302d4434d7e33701aa58df1e7
GolubeykovAnton <ds6580@yandex.ru>
2022-05-13 23:41:39 +0300

    Добавлен Segmented Picker для будущей вьюги создания рецепта

--- recipesBook/Views/ModifyRecipes/ModifyRecipeView.swift
+++ recipesBook/Views/ModifyRecipes/ModifyRecipeView.swift
@@ -10,12 +10,34 @@ import SwiftUI
 struct ModifyRecipeView: View {
     @Binding var recipe: Recipe
     
+    @State private var selection = Selection.main
+    
     var body: some View {
-        Button("Fill in the recipe with test data.") { recipe.mainInformation = MainInformation(name: "test", description: "test", author: "test", category: .breakfast)
-            recipe.directions = [Direction(description: "test", isOptional: false)]
-            recipe.ingredients = [Ingredient(name: "test", quantity: 1.0, unit: .none)]
+        
+        VStack {
+            Picker("Select recipe component", selection: $selection, content: {
+                Text("Main Info").tag(Selection.main)
+                Text("Ingredients").tag(Selection.ingredients)
+                Text("Directions").tag(Selection.directions)
+                
+            })
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            switch selection {
+            case .main:
+                Text("Main edit")
+            case .ingredients:
+                Text("Ingredients")
+            case .directions:
+                Text("Directions")
+            }
+            Spacer()
         }
-
+    }
+    enum Selection {
+        case main
+        case ingredients
+        case directions
     }
 }
 
